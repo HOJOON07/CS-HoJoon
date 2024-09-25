4e501de4ea1436bdb5e162bbcc699570f11beed9
hojoon <ghwns107@naver.com>
2024-09-25 21:55:23 +0900

    포트폴리오로 수정

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
   logo: <span>호준 기술지식 노트</span>,
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/HOJOON07",
   },
   sidebar: {
     defaultMenuCollapseLevel: 1,
