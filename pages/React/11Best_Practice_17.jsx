b64f8c1bf9503ac9bfea43838bffb9828ec1480a
hojoon <ghwns107@naver.com>
2024-10-03 23:44:07 +0900

    react best practice 정리중

---
+++ pages/React/11Best_Practice_17.jsx
@@ -0,0 +1,11 @@
+export const App = () => {
+  return (
+    <div>
+      <BackGroundHeader />
+      <div>
+        <main>main</main>
+        <button>todo button</button>
+      </div>
+    </div>
+  );
+};
